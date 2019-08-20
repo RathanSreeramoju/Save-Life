7c63df90c7f85480eedece41ade1daaabe691aac
ujwalsrinivas <52049916+ujwalsrinivas@users.noreply.github.com>
2019-08-20 17:07:08 -0400

    Search file

---
+++ Save-Life/src/Search.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['submit'])) {
+	$connection = new mysqli(host:"localhost", username:"root", passwd:"", dbname:"phpSearch");
+	$q = $connection->real_escape_string($_POST['q']);
+	$column = $connection->real_escape_string($_POST['column']);
+	if ($column == ""|| ($column !="firstName" && $column != "lastName"))
+	$column = "firstName";
+
+	$sql = $connection->query( query:"SELECT firstName FROM users WHERE $column LIKE '%$q%'");
+	if ($sql->num_rows > 0) {
+while ($data = $sql->fetch_array())
+    echo $data['firstName'] . "<br>";
+
+	} else
+	echo "Your search query doesn't match any data!";
+	
+}
